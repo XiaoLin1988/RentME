9627c76741e6f984ec7e612be9b085ea1c622425
xiaolin1988 <XiaoLin_1988@hotmail.com>
2017-12-14 14:05:05 +0800

    change structure

--- app/src/main/java/com/android/jianchen/rentme/activity/myprojects/events/ProjectCompleteEvent.java
+++ app/src/main/java/com/android/jianchen/rentme/activity/myprojects/events/ProjectCompleteEvent.java
@@ -1,14 +1,14 @@
-package com.android.jianchen.rentme.Events;
+package com.android.jianchen.rentme.activity.myprojects.events;
 
 import com.android.jianchen.rentme.model.rentme.ProjectModel;
 
 /**
  * Created by emerald on 12/10/2017.
  */
-public class ProjectCreateEvent {
+public class ProjectCompleteEvent {
     private ProjectModel project;
 
-    public ProjectCreateEvent(ProjectModel project) {
+    public ProjectCompleteEvent(ProjectModel project) {
         this.project = project;
     }
 
